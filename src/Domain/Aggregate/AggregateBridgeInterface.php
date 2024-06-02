e960fcf7463d49194a1e98acf5381cfc2f0a1a99
Simakin Dima <dima@foreline.ru>
2024-06-02 18:32:44 +0300

    feat: added AggregateBridgeInterface

---
+++ src/Domain/Aggregate/AggregateBridgeInterface.php
@@ -0,0 +1,12 @@
+<?php
+    declare(strict_types=1);
+    
+    namespace Domain\Aggregate;
+
+    /**
+     * Used for many-to-many relationship in a relational database, also known as an associative or junction table
+     */
+    interface AggregateBridgeInterface
+    {
+        
+    }
