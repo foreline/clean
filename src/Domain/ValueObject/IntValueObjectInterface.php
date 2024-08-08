52741f872b1f885566a1d7a5ab3547de8fdd43ca
Simakin Dima <dima@foreline.ru>
2024-08-08 13:10:42 +0300

    add: Value Objects Type Interfaces

---
+++ src/Domain/ValueObject/IntValueObjectInterface.php
@@ -0,0 +1,12 @@
+<?php
+    declare(strict_types=1);
+    
+    namespace Domain\ValueObject;
+
+    /**
+     * Interface that defines a Value Object (VO) whose value type is an integer.
+     */
+    interface IntValueObjectInterface
+    {
+        
+    }
