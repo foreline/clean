c49262476bd2d9869ba9df17f291e210fa8a9ef0
Simakin Dima <dima@foreline.ru>
2024-12-07 11:07:56 +0300

    feat: Repository Group

---
+++ src/Domain/File/Infrastructure/Repository/FileGroup.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Domain\File\Infrastructure\Repository;
+
+use Domain\Repository\Group;
+use Domain\Repository\GroupInterface;
+
+/**
+ *
+ */
+class FileGroup extends Group implements GroupInterface
+{
+    
+}
---
+++ src/Domain/User/Infrastructure/Repository/UserGroup.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Domain\User\Infrastructure\Repository;
+
+use Domain\Repository\Group;
+use Domain\Repository\GroupInterface;
+
+/**
+ *
+ */
+class UserGroup extends Group implements GroupInterface
+{
+    
+}
