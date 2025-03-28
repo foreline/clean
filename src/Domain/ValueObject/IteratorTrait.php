d9a7841e5c8cf4e7f5b8407c67e55445d4c998fc
Simakin Dima <dima@foreline.ru>
2025-03-28 07:04:54 +0300

    add: ValueObject Collection

---
+++ src/Domain/ValueObject/IteratorTrait.php
@@ -0,0 +1,98 @@
+<?php
+declare(strict_types=1);
+
+namespace Domain\ValueObject;
+
+/**
+ *
+ */
+trait IteratorTrait
+{
+    /** @var int  */
+    private int $position;
+    
+    /** @var ?ValueObjectInterface[] */
+    private ?array $items;
+    
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->position = 0;
+        $this->items = null;
+    }
+    
+    /**
+     * @return IteratorInterface
+     */
+    #[ReturnTypeWillChange]
+    public function next(): IteratorInterface
+    {
+        ++ $this->position;
+        return $this;
+    }
+    
+    /**
+     * @return int
+     */
+    public function key(): int
+    {
+        return $this->position;
+    }
+    
+    /**
+     * @return bool
+     */
+    public function valid(): bool
+    {
+        return isset($this->items[$this->position]);
+    }
+    
+    /**
+     * @return IteratorInterface
+     */
+    #[ReturnTypeWillChange]
+    public function rewind(): IteratorInterface
+    {
+        $this->position = 0;
+        return $this;
+    }
+    
+    /**
+     * @return int
+     */
+    public function getCount(): int
+    {
+        return ( null !== $this->items ? count($this->items) : 0 );
+    }
+    
+    /**
+     * @param ValueObjectInterface $item
+     * @return bool
+     */
+    public function has(ValueObjectInterface $item): bool
+    {
+        if ( null === !$this->items ) {
+            return false;
+        }
+        
+        return in_array($item, $this->items);
+    }
+    
+    /**
+     * @param $sortFunction
+     */
+    public function sort($sortFunction): void
+    {
+        if ( !is_callable($sortFunction) ) {
+            return;
+        }
+        
+        if ( !$this->valid() ) {
+            return;
+        }
+        
+        usort($this->items, $sortFunction);
+    }
+}
