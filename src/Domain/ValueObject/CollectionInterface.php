4ae0c0ea8453ea308073688bd23763df60b40da7
Simakin Dima <dima@foreline.ru>
2025-05-05 16:30:43 +0300

    fix: returned VO Collection interface and trait

---
+++ src/Domain/ValueObject/CollectionInterface.php
@@ -0,0 +1,83 @@
+<?php
+declare(strict_types=1);
+
+namespace Domain\ValueObject;
+
+use Iterator;
+use ReturnTypeWillChange;
+
+/**
+ * Flexible version of \Iterator Interface for ValueObjects
+ */
+interface CollectionInterface extends Iterator
+{
+    /**
+     * Return the current element
+     * @link https://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     */
+    public function current(): mixed;
+    
+    /**
+     * Move forward to next element
+     * @link https://php.net/manual/en/iterator.next.php
+     * @return self
+     */
+    #[ReturnTypeWillChange]
+    public function next(): self;
+    
+    /**
+     * Return the key of the current element
+     * @link https://php.net/manual/en/iterator.key.php
+     * @return mixed TKey on success, or null on failure.
+     */
+    public function key(): mixed;
+    
+    /**
+     * Checks if current position is valid
+     * @link https://php.net/manual/en/iterator.valid.php
+     * @return bool The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     */
+    public function valid(): bool;
+    
+    /**
+     * Rewind the Iterator to the first element
+     * @link https://php.net/manual/en/iterator.rewind.php
+     * @return self
+     */
+    #[ReturnTypeWillChange]
+    public function rewind(): self;
+    
+    /**
+     * Returns elements count
+     * @return int
+     */
+    public function getCount(): int;
+    
+    /**
+     * @return mixed
+     */
+    public function getCollection(): mixed;
+    
+    /**
+     * Add element to collection
+     * @param ValueObjectInterface $item
+     * @return self
+     */
+    public function addItem(ValueObjectInterface $item): self;
+    
+    /**
+     * Add elements to collection
+     * @param CollectionInterface $items
+     * @return $this
+     */
+    public function addItems(CollectionInterface $items): self;
+    
+    /**
+     * @param ?Iterator $items
+     * @return $this
+     */
+    public function setItems(?Iterator $items): self;
+    
+}
---
+++ src/Domain/ValueObject/CollectionTrait.php
@@ -0,0 +1,100 @@
+<?php
+declare(strict_types=1);
+
+namespace Domain\ValueObject;
+
+use ReturnTypeWillChange;
+
+/**
+ * Collection trait for ValueObjects
+ */
+trait CollectionTrait
+{
+    /** @var int  */
+    private int $position;
+    
+    /** @var ?ValueObjectInterface[] */
+    private ?array $items;
+    
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->position = 0;
+        $this->items = null;
+    }
+    
+    /**
+     * @return CollectionInterface
+     */
+    #[ReturnTypeWillChange]
+    public function next(): CollectionInterface
+    {
+        ++ $this->position;
+        return $this;
+    }
+    
+    /**
+     * @return int
+     */
+    public function key(): int
+    {
+        return $this->position;
+    }
+    
+    /**
+     * @return bool
+     */
+    public function valid(): bool
+    {
+        return isset($this->items[$this->position]);
+    }
+    
+    /**
+     * @return CollectionInterface
+     */
+    #[ReturnTypeWillChange]
+    public function rewind(): CollectionInterface
+    {
+        $this->position = 0;
+        return $this;
+    }
+    
+    /**
+     * @return int
+     */
+    public function getCount(): int
+    {
+        return ( null !== $this->items ? count($this->items) : 0 );
+    }
+    
+    /**
+     * @param ValueObjectInterface $item
+     * @return bool
+     */
+    public function has(ValueObjectInterface $item): bool
+    {
+        if ( null === !$this->items ) {
+            return false;
+        }
+        
+        return in_array($item, $this->items);
+    }
+    
+    /**
+     * @param $sortFunction
+     */
+    public function sort($sortFunction): void
+    {
+        if ( !is_callable($sortFunction) ) {
+            return;
+        }
+        
+        if ( !$this->valid() ) {
+            return;
+        }
+        
+        usort($this->items, $sortFunction);
+    }
+}
