52c24c0539d78e2a0612523c180d40ea1763134f
Simakin Dima <dima@foreline.ru>
2026-02-03 15:29:15 +0300

    feat: PascalCase support, comments

--- src/Domain/Helpers/Utils.php
+++ src/Domain/Helpers/Utils.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 namespace Domain\Helpers;
 
 /**
- *
+ * Utility class for string transformations, such as converting between camelCase, PascalCase, and snake_case.
  */
 class Utils
 {
     /**
+     * Converts camelCase or PascalCase to snake_case.
+     * For example, "camelCase" becomes "camel_case", and "PascalCase" becomes "pascal_case".
+     * If the input string contains consecutive uppercase letters, they are treated as a single word. For example,
+     * "XMLHttpRequest" becomes "xml_http_request".
+     *
      * @param string $input
      * @param bool $strtoupper
      * @return string
@@ -30,6 +35,27 @@ class Utils
     }
     
     /**
+     * Converts PascalCase to snake_case.
+     * For example, "PascalCase" becomes "pascal_case".
+     * If the input string contains consecutive uppercase letters, they are treated as a single word. For example,
+     * "XMLHttpRequest" becomes "xml_http_request".
+     *
+     * @param string $input
+     * @param bool $strtoupper
+     * @return string
+     */
+    public static function pascalToSnake(string $input, bool $strtoupper = false): string
+    {
+        return self::camelToSnake($input, $strtoupper);
+    }
+    
+    /**
+     * Converts snake_case to camelCase.
+     * For example, "snake_case" becomes "snakeCase". If the second parameter is set to true,
+     * the first character will be capitalized, resulting in "SnakeCase".
+     * If the input string contains consecutive underscores, they are treated as a single separator. For example,
+     * "snake__case" becomes "snakeCase" or "SnakeCase".
+     *
      * @param string $string
      * @param bool $capitalizeFirstCharacter
      * @return string
@@ -48,6 +74,23 @@ class Utils
     }
     
     /**
+     * Converts snake_case to PascalCase.
+     * For example, "snake_case" becomes "SnakeCase". If the input string contains consecutive underscores,
+     * they are treated as a single separator. For example, "snake__case" becomes "SnakeCase".
+     *
+     * @param string $string
+     * @return string
+     */
+    public static function snakeToPascal(string $string): string
+    {
+        return self::snakeToCamel($string, true);
+    }
+    
+    /**
+     * Converts the first character of a string to lowercase, taking into account multibyte characters.
+     * If the second character of the string is uppercase, the original string is returned unchanged. For example,
+     * "XMLHttpRequest" remains "XMLHttpRequest", while "HelloWorld" becomes "helloWorld".
+     *
      * @param string $string
      * @return string
      */
